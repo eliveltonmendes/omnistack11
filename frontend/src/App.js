dcbaa802021edf34fd1d6778e0b4c2049634d9e0
Elivelton Mendes <eliveltonmendes14@gmail.com>
2020-07-05 21:32:51 -0300

    Remoção código mobile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import './global.css'
-
-import Routes from './routes'
+import Logon from './pages/Logon';
 
 function App() {
   return (
-    <Routes />    
-    
+    <Logon />
   );
 }
 
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,56 +1,7 @@
-import React, { useState } from 'react'
-import { Link, useHistory } from 'react-router-dom'
-import { FiLogIn } from 'react-icons/fi'
+import React from 'react';
 
-import api from '../../services/api'
-
-import './styles.css'
-
-import logoImg from '../../assets/logo.svg'
-import heroesImg from '../../assets/heroes.png'
-
-export default function Logon() {
-   const [id, setId] = useState('')
-   const history = useHistory()
-
-   async function handleLogin(event) {
-      event.preventDefault()
-
-      try {
-         const response = await api.post('sessions', { id })
-
-         localStorage.setItem('ongId', id)
-         localStorage.setItem('ongName', response.data.name)
-
-         history.push('/profile')
-      } catch(error) {
-         alert(error)
-      }
-   }
-
-   return (
-      <div className="logon-container">
-         <section className="form">
-            <img src={logoImg} alt="Be The Hero"/>
-
-            <form onSubmit={handleLogin}>
-               <h1>Faça seu logon</h1>
-
-               <input 
-                  placeholder="Sua ID" 
-                  value={id}
-                  onChange={e => setId(e.target.value)}
-               />
-               <button className="button" type="submit">Entrar</button>
-
-               <Link className="back-link" to="/register">
-                  <FiLogIn size={16} />
-                  Não tenho cadastro
-               </Link>
-            </form>
-         </section>
-
-         <img src={heroesImg} alt="Heroes" />         
-      </div>
-   )
+export default function logon(){
+    return (
+        <h1>Hello Logon</h1>
+    )
 }
