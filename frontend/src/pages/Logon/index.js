f653dba4646b5e83a7854d77c573044a928e74ef
Elivelton Mendes <eliveltonmendes14@gmail.com>
2020-07-10 13:44:59 -0300

    Criação de tela de profile

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -16,7 +16,7 @@ export default function logon(){
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua Id"/>
                     <button className="button" type="submit">Entrar</button>
-                    <Link to="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Não tenho cadastro
                     </Link>
